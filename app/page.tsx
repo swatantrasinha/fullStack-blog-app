b542393750c006fda38641031ec408f3ab034198
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2023-10-17 09:54:01 +0530

    commit 9

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
  async function fetchBlogs() {
-  const apiUrl= process.env.API_URL;
+  const apiUrl= process.env.VERCEL_URL;
   const res= await fetch(`${apiUrl}/api/blog/`, {
     next: {
       revalidate: 10,
